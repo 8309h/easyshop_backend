509b1cfa1da20ec8da04f56b4537cb010fc9e8f5
harshal <harshalwagh201718@gmail.com>
2025-12-03 18:18:16 +0530

    added product schema

--- models/Product.model.js
+++ models/Product.model.js
@@ -1,32 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const MenProductSchema = new mongoose.Schema({
-//     Title: { type: String, required: true },
-//     Category: String, // Adjusted spelling to match the provided data
-//     Image: { type: String, required: true },
-//     Description: { type: String, required: true },
-//     Price: { type: Number, required: true },
-// });
-
-// const WomenProductSchema = new mongoose.Schema({
-//   Title: { type: String, required: true },
-//   Catogory: String, // Make sure Catogory is spelled correctly
-//   Image: { type: String, required: true },
-//   Description: { type: String, required: true },
-//   Price: { type: Number, required: true },
-//   // Add other fields specific to women's products
-// });
-
-// const MenProductModel = mongoose.model("MenProduct", MenProductSchema);
-// const WomenProductModel = mongoose.model("WomenProduct", WomenProductSchema);
-
-// module.exports = {
-//   MenProductModel,
-//   WomenProductModel,
-// };
-
-
-
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
@@ -45,7 +16,7 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }
 );
 // ADD THIS FOR SEARCH TO WORK
-ProductSchema.index({
+productSchema.index({
   title: "text",
   description: "text",
   category: "text"
